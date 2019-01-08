c0537001144aabbc1b75c0397ae23ed7945f5680
doniyor2109 <doniyor2109@gmail.com>
2019-01-08 12:58:45 +0500

    fix: Missing values assign

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ const [name, value] = process.argv.slice(2);
 let values = { [name]: value };
 
 if (~name.indexOf('.')) {
-  name.split('.').reduce(function(obj, key, index, arr) {
+  values = name.split('.').reduce(function(obj, key, index, arr) {
     return (
       obj[key] = ~(index-arr.length) ? value: {},
       obj
